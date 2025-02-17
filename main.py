6b236dc7a9c26b645032009bebdb9d7b5e408d69
Avyank Niraj <159672896+a-niraj@users.noreply.github.com>
2025-02-16 17:46:56 -0800

    Adding files

---
+++ main.py
@@ -0,0 +1,10 @@
+from LSBSteg import LSBSteg
+
+import cv2
+import docopt
+import numpy as np
+
+steg = LSBSteg(cv2.imread("/Users/avyankniraj/pythonSteg/image1.png"))
+img_encoded = steg.encode_text("secret")
+cv2.imwrite("my_new_image.png", img_encoded)
+
