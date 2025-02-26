5a6ffd224d10accc53e6072c6066bc5867006e11
Avyank Niraj <159672896+a-niraj@users.noreply.github.com>
2025-02-25 21:21:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ steg = LSBSteg(cv2.imread("/Users/avyankniraj/pythonSteg/image1.png"))
 img_encoded = steg.encode_text("secret")
 cv2.imwrite("my_new_image.png", img_encoded)
 
+im = cv2.imread("my_new_image.png")
+steg = LSBSteg(im)
+print("Text value: ",steg.decode_text())
